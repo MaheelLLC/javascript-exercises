f1106a1a471ad36be5c6af472574923f40053540
Maheel Luqman <maheelllc@gmail.com>
2024-01-29 20:41:58 -0500

    Completed the first seven exercises
    I finished the 4th JavaScript Fundamental lesson on Odin Project. These
    seven exercises that I have modified serve as proof of completion.
    Please trust me. I did them myself.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
-
+/*
+const removeFromArray = function(array, ...args) {
+    let arg;
+    for (arg of args) {
+        if (array.includes(arg)) {
+            array.splice(array.indexOf(arg), 1);
+        }
+    }
+    return array;
 };
-
+*/
+/*
+The filter method works by passing each element of an array
+by passing each element of an array through a test function.
+This test function is the arrow function (!args.includes(item) 
+in this case). Futhermore, this test function should return a 
+boolean value. If the function returns true, that element is kept 
+and included in the new array. If the function returns false, the 
+element gets excluded (or filtered out).
+*/
+const removeFromArray = function(originalArray, ...args) {
+    return originalArray.filter(item => !args.includes(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!(typeof num1 === "number" && 
+          typeof num2 === "number")) {
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    let smallNum = num1 < num2 ? num1 : num2;
+    let largeNum = num1 > num2 ? num1 : num2;
+
+    let fibSmall = Math.floor((smallNum) * (smallNum + 1) / 2);
+    let fibLarge = Math.floor((largeNum) * (largeNum + 1) / 2);
+
+    return fibLarge - fibSmall + smallNum
+    
 
 };
+
+// const sumAll = function(num1, num2) {
+//     if (!(typeof num1 === "number" && 
+//           typeof num2 === "number")) {
+//         return "ERROR";
+//     }
+//     if (num1 < 0 || num2 < 0) {
+//         return "ERROR";
+//     }
+
+//     let finalSum = 0;
+//     let smallNum = num1 < num2 ? num1 : num2;
+//     let largeNum = num1 > num2 ? num1 : num2;
+    
+//     for (let i = smallNum; i <= largeNum; i++) {
+//         finalSum += i;
+//     }
+//     return finalSum;
+
+// };
 
 // Do not edit below this line
 module.exports = sumAll;
